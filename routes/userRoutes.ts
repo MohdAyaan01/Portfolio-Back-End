b1a00ff67d0a0e4769046434c72f730b04db408d
Mohd Ayaan Siddiqui <zxayaan2.0@gmail.com>
2026-09-22 14:24:13 +0530

    feat: add user routes for authentication, profile, and history management

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -2,7 +2,8 @@ import express from "express";
 import {getUserHistory, Login,Logout,SignUp, getMe} from "../controller/userController.js";
 import { googleAuth } from "../controller/googleController.js";
 import isAuthenticated from "../middleware/isAuth.js";
-
+import { validate } from "../validation/validate.js";
+import { SignUpSchema } from "../validation/authValidation.js";
 const router = express.Router();
 
 router.route("/signup").post(SignUp);
@@ -11,5 +12,6 @@ router.route("/logout").post(Logout);
 router.route("/google-login").post(googleAuth)
 router.get("/me", isAuthenticated, getMe);
 router.get("/history/:userId", getUserHistory);
+router.post("/signup",validate(SignUpSchema),SignUp);
 export default router
 ;
